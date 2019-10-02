2fb7c105544abbc28abaa74dd7917114a3e320d9
Heather Walls <wallshkim@gmail.com>
2019-10-01 21:27:45 -0500

    Adds button that when clicked runs function to put output on DOM

--- client.js
+++ client.js
@@ -1,3 +1,5 @@
+$(document).ready(readyNow);
+
 const employees = [
   {
     name: 'Atticus',
@@ -40,13 +42,36 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(employees);
 
-for (let i = 0; i < employees.length; i++) {
-  console.log(employeeBonus(employees[i]));
-}// end for
+function readyNow() {
+  console.log('in readyNow');
+  $('#runButton').on('click', handleClick);
+}// end readyNow
 
-function employeeBonus(employee){
+function handleClick(){
+  for (let i = 0; i < employees.length; i++) {
+    console.log(employeeBonus(employees[i]));
+  }// end for
+}
+
+function displayEmployeeBonus(employee) {
+  console.log('in displayEmployeeBonus');
+  // target element
+  let employeeOutput = $('#employeeOutput');
+  // append to DOM
+  employeeOutput.append(`
+  <tr>
+    <td>${employee.name}</td>
+    <td>${employee.bonusPercentage}</td>
+    <td>${employee.totalBonus}</td>
+    <td>${employee.totalCompensation}</td>
+  </tr>`);
+}// end displayEmployeeBonus
+
+function employeeBonus(employee) {
+  console.log('in employeeBonus');
+  
   // define empty object
   let newEmployeeObject = {};
   // get employee's name
@@ -63,14 +88,18 @@ function employeeBonus(employee){
   let totalCompensation = Number(employee.annualSalary) + totalBonus;
   // add to new object
   newEmployeeObject.totalCompensation = totalCompensation
-  // return
+  // display on DOM
+  displayEmployeeBonus(newEmployeeObject);
+  // return object
   return newEmployeeObject;
 }// end employeeBonus
 
-function bonusCalculation(employee){
+function bonusCalculation(employee) {
+  console.log('in bonusCalculation');
+  
   let bonusPercentage = 0;
 
-  if(employee.reviewRating === 3){
+  if (employee.reviewRating === 3) {
     bonusPercentage += .04
   }// end rating of 3
   else if (employee.reviewRating === 4) {
@@ -80,21 +109,21 @@ function bonusCalculation(employee){
     bonusPercentage += .1
   }// end rating of 5
 
-  if(employee.employeeNumber.length === 4){
+  if (employee.employeeNumber.length === 4) {
     bonusPercentage += .05;
   }// end employee # four digits long
 
-  if(Number(employee.annualSalary) > 65000){
+  if (Number(employee.annualSalary) > 65000) {
     bonusPercentage -= .01;
   }// end employee annual income greater than $65000
 
-  if(bonusPercentage > .13){
+  if (bonusPercentage > .13) {
     bonusPercentage = .13;
-  }
+  }// end employee bonusPercentage over .13
 
   if (bonusPercentage < 0) {
     bonusPercentage = 0;
-  }
+  }// end employee bonusPercentage below 0
 
   return bonusPercentage;
 }// end bonusCalculation
